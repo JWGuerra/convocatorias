d73939a3705c46eda22434bc75f1a3a9bfe9f389
waldir <171910@unsaac.edu.pe>
2023-11-29 11:56:49 -0500

    Ajustes Finales

--- convocatoria.php
+++ convocatoria.php
@@ -10,10 +10,9 @@
                 <div class="col-sm-4 info-blocks">
                     <i class="icon-info-blocks fa fa-briefcase"></i>
                     <div class="info-blocks-in">
-                        <h3><?php echo '<a href="' . web_root . 'index.php?q=hiring&search=' . $convocatoria->CONVOCATORIA. '">' . $convocatoria->CONVOCATORIA. '</a>'; ?></h3>
+                        <h3><?php echo '<a href="' . web_root . 'index.php?q=hiring&search=' . $convocatoria->CONVOCATORIA . '">' . $convocatoria->CONVOCATORIA . '</a>'; ?></h3>
                     </div>
                 </div>
-
             <?php } ?>
         </div>
     </div>
